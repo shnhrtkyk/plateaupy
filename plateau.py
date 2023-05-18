79c32fcfdc5926e7711c93d15cf664ef09935905
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2023-05-18 09:47:15 +0900

    Add files via upload

---
+++ plateau.py
@@ -0,0 +1,16 @@
+import plateaupy  
+import pprint
+
+path = "/home/map4/Downloads/23100_nagoya-shi_2020_citygml_4_op"
+pl = plateaupy.plparser(paths=[path])  
+pl.loadFiles()
+
+print(pl.bldg)
+print(len(pl.bldg))
+print(pl.bldg.keys())
+mylist = list(pl.bldg.keys())
+print(mylist) 
+print(pl.bldg[mylist[0]].buildings[0].getLOD0Footprintpolygons())
+
+# pprint.pprint(dir(pl.bldg[mylist[0]]))
+# pprint.pprint(vars(pl.bldg[mylist[0]]))
